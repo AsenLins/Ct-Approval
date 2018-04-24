b5e1d85e239ff5e0842a423e23e00a9fa804dd75
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2018-04-24 18:13:08 +0800

    update

--- src/singleApp/App.vue
+++ src/singleApp/App.vue
@@ -1,11 +1,63 @@
 <template>
 <el-container class="e-container" >
-  <el-header class="e-header">Header</el-header>
+  
+  <el-header height="60px" class="e-header">
+      <el-row class="ap-title" type="flex"  align="middle">
+           
+           <img src="./assets/image/logo.png" />
+           
+           <el-row type="flex" style="color:gray;" align="bottom">
+               移动审批
+               <small>Approval.Admin</small>
+            </el-row>       
+      </el-row>
+      
+  </el-header>
   <el-container>
-    <el-aside class="e-menu" width="200px">Aside</el-aside>
-    <el-main class="e-main"> </el-main>
+     <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo ap-menut"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>     
+
+    <el-main class="e-main"> 
+        this is Content
+    </el-main>
   </el-container>
 </el-container>
+
+
 </template>
 
 <script>
@@ -16,7 +68,7 @@ export default{
     data(){
         return {
             title:"使用例子442424",
-            msg:"vvvbbbbbb11444331213123124444323123444444444444442444444bbbbbbqqqqqqqqqqqqqqqqqqqqqqqq"
+            msg:""
         }
     }
 }
@@ -35,13 +87,20 @@ html,body{
     height: 100%;
 }
 .e-header{
-     background:linear-gradient(to right, #ed2739 , #f03d4d); /* 标准的语法 */
-     color:white;
-     height: 80px;
-     line-height: 80px;
+    box-shadow: 1px -4px 20px 1px gray;
+    z-index: 2;
 }
 .e-menu{
 
+}
+.ap-title{
+    height: 100%;
+}
+.ap-title img{
+    max-height: 38px;
+}
+.ap-menut{
+    width: 200px;
 }
 
 </style>
--- src/singleApp/main.js
+++ src/singleApp/main.js
@@ -4,22 +4,24 @@ import VueRouter from 'vue-router'
 
 
 /*按需引入element-ui*/
-import {Aside,Header,Main,Container} from '../lib/elementUI.Lib';
-/*注册组件*/
-console.log("Aside.name",Aside.name);
+/*
+import { Aside, Header, Main, Container } from 'element-ui';
+
+console.log("Aside.name", Aside.name);
 Vue.component(Aside.name, Aside);
 Vue.component(Header.name, Header);
 Vue.component(Main.name, Main);
-Vue.component(Container.name,Container);
+Vue.component(Container.name, Container);
+*/
 
+import ElementUI from 'element-ui';
+import './style/elementUI/index.css';
 
-
-
-
+Vue.use(ElementUI);
 Vue.use(VueRouter);
 const Foo = { template: '<div>foo </div>' }
 const Bar = { template: '<div>bar</div>' }
-const MoreBar={template: '<div><router-view ></router-view></div>'}
+const MoreBar = { template: '<div><router-view ></router-view></div>' }
 
 
 
@@ -29,26 +31,26 @@ const MoreBar={template: '<div><router-view ></router-view></div>'}
 // 或者，只是一个组件配置对象。
 // 我们晚点再讨论嵌套路由。
 const routes = [
-  { path: '/foo', components: [Foo,Bar] },
-  { path: '/bar', component: Bar },
-  {
-    path:'/morebar',
-      component:{template:"<div>more bar view <router-view name='a' ></router-view> <router-view name='b' ></router-view> </div>"},
+    { path: '/foo', components: [Foo, Bar] },
+    { path: '/bar', component: Bar },
+    {
+        path: '/morebar',
+        component: { template: "<div>more bar view <router-view name='a' ></router-view> <router-view name='b' ></router-view> </div>" },
 
-      children:[{
-      path:"/morebar",
-      components:{
-        a:{template:"<div>test post</div>"},
-        b:{template:"<div>i am the two</div>"}
-      }
-      }]
-  }
+        children: [{
+            path: "/morebar",
+            components: {
+                a: { template: "<div>test post</div>" },
+                b: { template: "<div>i am the two</div>" }
+            }
+        }]
+    }
 ]
 
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
+    routes // （缩写）相当于 routes: routes
 })
 
 
@@ -56,7 +58,7 @@ const router = new VueRouter({
 
 
 new Vue({
-  router:router,
-  el: '#App',
-  render: h => h(App)
+    router: router,
+    el: '#App',
+    render: h => h(App)
 }).$mount('#App');
